fe185d5175fe8280ff601c7fdae20984eb4c8956
Dane Cronin <danecronin@gmail.com>
2022-08-15 16:04:45 -0600

    added comments to html and pseudo-coded .js script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
 //Declare variables 
 
 
-// create a function to display a daily schedule that shows hourly time blocks
+// ** create a function to display a daily schedule that shows hourly time blocks **//
 
-// create a function so that
+// create a function to create/generate rows to fill daily schedule in hour-rows
+
+// create a function to generate hour time block for each row of daily schedule
+
+
+// function to add and save tasks by time
+
+
+// function to edit and delete tasks
+
+// function to check if current row time is past, present or future compared to hours-rows
+
+
+// update and color- code Hour-row/tasks based on whether it is past, current or a future task
+
+// create function to store row to local storage
